d63e74d04417ac81f47f729448bb9fdfd3f82e49
Jehanzaib <jehanzaib.dev@gmail.com>
2026-04-04 21:09:02 +0500

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const helmet=require("helmet");
+const morgan=require("morgan");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+
 
 app.listen(8800,()=>{
 	console.log("server running successfully");

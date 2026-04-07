5d06163276ab69201bb34d124d92088b6f7dea3d
Jehanzaib <jehanzaib.dev@gmail.com>
2026-04-07 16:50:34 +0500

    add mongoose connection in the index.js file

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ const helmet=require("helmet");
 const morgan=require("morgan");
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL);
+
+const connectDB= async()=>{
+    try{
+        const connectionInstance=await mongoose.connect(process.env.MONGO_URL);
+        console.log(`mongoDB connected !!! ${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.log("error occured", error);
+        process.exit(1);
+    }
+}
 
 
 app.listen(8800,()=>{
 	console.log("server running successfully");
 })
+
+connectDB();
